9ab8d66bc1729347c275a3f68513f6f9b84c3a38
Nam Hoang Van (particle4dev) <particle4dev@gmail.com>
2017-07-11 08:32:39 +0700

    step 3: update service worker

--- sw.js
+++ sw.js
@@ -1,22 +1,36 @@
+const expectedCaches = ['static-v2'];
+
 self.addEventListener('install', event => {
-  console.log('V1 installing…');
-  // cache a cat PNG
+  console.log('V2 installing…');
+
+  // cache a horse png into a new cache, static-v2
   event.waitUntil(
-    caches.open('static-v1').then(cache => cache.add('/cat.png'))
+    caches.open(expectedCaches).then(cache => cache.add('/horse.png'))
   );
 });
 
 self.addEventListener('activate', event => {
-  console.log('V1 now ready to handle fetches!');
+  // delete any caches that aren't in expectedCaches
+  // which will get rid of static-v1
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!expectedCaches.includes(key)) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log('V2 now ready to handle fetches!');
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
 
-  // serve the cat PNG from the cache if the request is
+  // serve the horse png from the cache if the request is
   // same-origin and the path is '/dog.png'
-  console.log(url);
   if (url.origin == location.origin && url.pathname == '/dog.png') {
-    event.respondWith(caches.match('/cat.png'));
+    event.respondWith(caches.match('/horse.png'));
   }
 });
